79a8ba1761e2ec3b7058f161ea7119b4a3377588
Viet Ha Ho <viet_ha.ho115@web.de>
2016-04-27 16:23:03 +0900

    added Rectangle and ShapeTest

---
+++ Lab6_team_prince197/src/ShapeTest.java
@@ -0,0 +1,26 @@
+
+public class ShapeTest {
+
+	public void ShapeTest () {
+		
+	}
+	
+	public void start () {
+		
+	}
+	
+	
+	
+	
+	
+	
+	public static void main (String[] args) {
+		
+	}
+	
+	public void processShape (Shape shape) {
+		
+	}
+	
+
+}
