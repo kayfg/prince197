fe9aa92a8322f1f28c8b32870e356cf5263f0439
Viet Ha <viet_ha.ho115@web.de>
2016-04-29 05:26:05 +0200

    blabla

--- Lab6_team_prince197/src/ShapeTest.java
+++ Lab6_team_prince197/src/ShapeTest.java
@@ -1,12 +1,20 @@
 
 public class ShapeTest {
-
+//Kommentar
 	public ShapeTest () {
 		
 	}
 	
 	public void start () {
+	
+		Shape[] shape = new Shape[3];
 		
+		shape[0] = new Rectangle();
+		shape[1] = new Circle();
+		shape[2] = new Triangle();
+		
+		for (x: shape) 
+			x.processShape();
 	}
 	
 	
@@ -16,10 +24,19 @@ public class ShapeTest {
 	
 	public static void main (String[] args) {
 		
+		ShapeTest s = new ShapeTest();
+		
+		s.start();
+		
 	}
 	
 	public void processShape (Shape shape) {
 		
+		 shape.printInfo();
+		 
+		 if (shape instanceof Rectangle) {
+			 shape.drawRectanle();
+		 }
 	}
 	
 
